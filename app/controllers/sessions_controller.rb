1226b3e651cfa584658b2e71355d2770eaa86b88
anandiyer <truecalling9@gmail.com>
2011-05-16 17:52:31 -0700

    minor changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    p "referer = " + request.referer
-    p "requesturi = " + request.request_uri
-    p "requesturl = " + request.url
-    
     session[:return_to] = request.referer
 
-    auth = request.env['rack.auth']
+    auth = request.env["omniauth.auth"]
     unless @auth = Authorization.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
       @auth = Authorization.create_from_hash(auth, current_user)
-      omniauth = request.env["omniauth.auth"]
-      @auth.user.user_profile_pic_url = "https://graph.facebook.com/"+omniauth['uid']+"/picture"
     end
     # Log the authorizing user in.
     self.current_user = @auth.user
