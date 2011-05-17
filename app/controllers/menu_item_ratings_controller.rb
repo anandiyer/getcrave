f5f84f072092b30bd372bdc925bbe7bad0284ae6
almazom <almazom@localhost.localdomain>
2011-05-17 19:32:47 +0400

    fix: adding menu items rating and review

--- app/controllers/menu_item_ratings_controller.rb
+++ app/controllers/menu_item_ratings_controller.rb
@@ -91,7 +91,7 @@ class MenuItemRatingsController < ApplicationController
         format.json { render :json => @menu_item_rating, :status => :created, :location => @menu_item_rating }
         format.js {
 #          TODO: change temp number to real
-          if params[:menu_item_rating][:rating]
+          if !params[:menu_item_rating][:rating].empty?
             message = "You rated this dish with <b>"+params[:menu_item_rating][:rating]+"</b> stars!"
           else
             message = "Your review was successfully added"
