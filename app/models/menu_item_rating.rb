dc5501e17f5cd587f5152faebd9261835d9750b7
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-05-22 14:21:57 -0700

    removing counter_cache from menu_item_rating from menu_item

--- app/models/menu_item_rating.rb
+++ app/models/menu_item_rating.rb
@@ -1,6 +1,6 @@
 class MenuItemRating < ActiveRecord::Base
     belongs_to  :menu_item
-    belongs_to  :user, :counter_cache => true
+    belongs_to  :user, :counter_cache => :user_ratings_count
     belongs_to  :restaurant
     has_many    :menu_item_rating_helpfuls
 end
