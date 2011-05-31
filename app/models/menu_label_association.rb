1437e6a52f45a94098006580e8b829afacd958cb
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-05-31 16:13:13 -0700

    adding validates_uniqueness_of to MenuItemRatingHelpful and MenuLabelAssociation

--- app/models/menu_label_association.rb
+++ app/models/menu_label_association.rb
@@ -1,3 +1,5 @@
 class MenuLabelAssociation < ActiveRecord::Base
   belongs_to  :menu_item
+  validates_uniqueness_of :menu_item_id, :scope => [:menu_label_id, :user_id]
+  validates_presence_of :menu_item_id, :menu_label_id, :user_id
 end
