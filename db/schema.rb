61a0b9ada9eafb52603d57ceb210546682f28434
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-06-07 16:33:59 -0700

    rake db:migrate required - saving the users facebook profile pic url and the email address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110603234401) do
+ActiveRecord::Schema.define(:version => 20110607231523) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -186,6 +186,7 @@ ActiveRecord::Schema.define(:version => 20110603234401) do
     t.datetime "updated_at"
     t.string   "user_profile_pic_url"
     t.integer  "user_ratings_count"
+    t.string   "email"
   end
 
 end
