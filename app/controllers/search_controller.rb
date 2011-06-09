8b8597130a59d93298be88c25dff754ade21f398
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-06-09 11:00:10 -0700

    changing to search on menu_items by default

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,18 +3,11 @@ class SearchController < ApplicationController
   
   def index
     if (params[:q] && !params[:q].empty?)
-      if !is_almazom?
-        @search = Sunspot.search(Restaurant, MenuItem) do
+        @search = Sunspot.search(MenuItem) do
           fulltext(params[:q])
-          paginate :page => 1, :per_page => 100
+          paginate :page => 1, :per_page => 50
         end
         @results = @search.results
-      else
-
-#        TODO:Remove me on production
-        @results = MenuItem.where("id > 100 AND  id < 110")
-      end
-
 
       respond_to do |format|
         format.html
