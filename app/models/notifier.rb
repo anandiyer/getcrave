7cd72d759fa982347c0addbc5d9eba5d8953bcc9
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-06-10 17:16:43 -0700

    email

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,13 +3,16 @@ class Notifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that contains the user's email address
   def signup_email(user)
-    mail( :to => user.email, :subject => "Thanks for signing up for Crave!", :body => "Your friends at Crave" )
+    if (user.email && !user.email.empty?)
+      mail( :to => user.email, :subject => "Thanks for signing up for Crave!", :body => "Your friends at Crave" )
+    end
   end
   
   def review_helpful(current_user, to_user, menu_item)
-    @body = "Hey there, " + current_user.user_name + " found your Crave of " + menu_item.name + 
-      " helpful. Check it out. http://getcrave.com/menu_items/" + menu_item.id.to_s
-    
-    mail( :to => to_user.email, :subject => "Someone liked your Crave!", :body => @body)
+    if (to_user.email && !to_user.email.empty?)
+      @body = "Hey there, " + current_user.user_name + " found your Crave of " + menu_item.name + 
+        " helpful. Check it out. http://getcrave.com/menu_items/" + menu_item.id.to_s
+      mail( :to => to_user.email, :subject => "Someone liked your Crave!", :body => @body)
+    end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Crave::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app518646@heroku.com",
-  :password => "5b29d416c8d90bec0a",
-  :domain => "getcrave.com",
-  :address => "smtp.sendgrid.net",
-  :port => 25,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+ActionMailer::Base.smtp_settings = { 
+    :user_name => "app518646@heroku.com",
+    :password => "5b29d416c8d90bec0a",
+    :domain => "getcrave.com",
+    :address => "smtp.sendgrid.net",
+    :port => 25,
+    :authentication => :plain,
+    :enable_starttls_auto => true
 }
