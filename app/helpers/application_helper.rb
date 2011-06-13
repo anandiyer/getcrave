520019509e6a1edffcf6bb221bf1c5509e3011c3
almazom <almazom@localhost.localdomain>
2011-06-13 19:42:20 +0400

    Nearby: The nearby navigation item should have the background pressed state to show that it is selected. The nearby icon should then change to white.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,20 @@ module ApplicationHelper
     @controller.controller_name+"_"+@controller.action_name
   end
 
+  def navigation_selected act_name
+
+    class_name = "not"
+    class_name = "selected" if act_name == "nearby" && class_name_from_path == "restaurants_index"
+    class_name = "selected" if act_name == "saved" && class_name_from_path == "users_saved"
+    class_name = "selected" if act_name == "profile" && class_name_from_path == "users_show"
+
+#    act_name == "nearby" && class_name_from_path == "restaurants_index" ? "selected" : "notselected"
+
+    class_name.to_s
+
+
+  end
+
   def stars_class_normalize decimal_data
     decimal_data.to_s.gsub(/[.]/, '_')
   end
