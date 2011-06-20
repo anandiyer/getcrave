3ebc33015c3435a976ade98ba69d674462445ab5
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-06-19 19:15:42 -0700

    fixing the grouping of stars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,32 @@ module ApplicationHelper
 
   def stars_class_normalize decimal_data
     if (decimal_data.to_i != 0)
-      decimal_data = (decimal_data * 10).round.to_f / 10
+      dd = (decimal_data * 10).round.to_f / 10
+      decimal_data = dd.to_f
+      
+      if (decimal_data >= 0.0 && decimal_data <= 0.2)
+        decimal_data = 0.0
+      elsif (decimal_data >= 0.3 && decimal_data <= 0.7)
+        decimal_data = 0.5
+      elsif (decimal_data >= 0.8 && decimal_data <= 1.2)
+        decimal_data = 1.0
+      elsif (decimal_data >= 1.3 && decimal_data <= 1.7)
+        decimal_data = 1.5
+      elsif (decimal_data >= 1.8 && decimal_data <= 2.2)
+        decimal_data = 2.0
+      elsif (decimal_data >= 2.3 && decimal_data <= 2.7)
+        decimal_data = 2.5
+      elsif (decimal_data >= 2.8 && decimal_data <= 3.2)
+        decimal_data = 3.0
+      elsif (decimal_data >= 3.3 && decimal_data <= 3.7)
+        decimal_data = 3.5
+      elsif (decimal_data >= 3.8 && decimal_data <= 4.2)
+        decimal_data = 4.0
+      elsif (decimal_data >= 4.3 && decimal_data <= 4.7)
+        decimal_data = 4.5
+      elsif (decimal_data >= 4.8 && decimal_data <= 5.0)
+        decimal_data = 5.0
+      end
     end
     decimal_data.to_s.gsub(/[.]/, '_')
   end
