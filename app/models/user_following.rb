3426695cf662a7398c87ee113a3dc65743722d14
Anand Iyer <anandiyer@aiyers-Macbook-Pro.local>
2011-06-21 23:10:32 -0700

    uniqueness of user_saved_menu_item and user_following

--- app/models/user_following.rb
+++ app/models/user_following.rb
@@ -1,3 +1,4 @@
 class UserFollowing < ActiveRecord::Base
   belongs_to :user
+  validates_uniqueness_of :following_user_id, :scope => :user_id, :message => "this user is already being followed"
 end
--- app/models/user_saved_menu_item.rb
+++ app/models/user_saved_menu_item.rb
@@ -1,4 +1,5 @@
 class UserSavedMenuItem < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :menu_item
+  validates_uniqueness_of :menu_item_id, :scope => :user_id, :message => "this item has already been saved"
 end
