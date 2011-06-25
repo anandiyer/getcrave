752ca0c755f6c722230c639ef8073ffe00e4880b
Aaron Hans <ahans@ign.com>
2011-06-25 16:13:54 -0700

    add secret logout mechanism for testing: click on picture on profile tab

--- public/mobile/session.js
+++ public/mobile/session.js
@@ -4,11 +4,11 @@ function justLoggedIn() {
     if(myUID!="" && myUID!=null) {
         console.log('found');
         $(".loginButton").css("display","none");
-        //Ext.getCmp("logoutButton").setVisible(true);
+        //$(".logoutButton").css("display","block");
     } else {
         console.log('not found');
         $(".loginButton").css("display","block");
-        //Ext.getCmp("logoutButton").setVisible(false);
+        //$(".logoutButton").css("display","none");
     }    
     // ?get uid from local storage here?
     //have to reach docked bottom bar, change to sign out, display form links
