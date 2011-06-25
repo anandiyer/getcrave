1fca2b352c621c1f58935fddbebbb3592b5891b4
Aaron Hans <ahans@ign.com>
2011-06-25 15:55:46 -0700

    sign in button if signed out

--- public/mobile/session.js
+++ public/mobile/session.js
@@ -1,17 +1,15 @@
 
 function justLoggedIn() {
     myUID = localStorage.getItem("uid");
-    /*
     if(myUID!="" && myUID!=null) {
         console.log('found');
-        Ext.getCmp("loginButton").setVisible(false);
-        Ext.getCmp("logoutButton").setVisible(true);
+        $(".loginButton").css("display","none");
+        //Ext.getCmp("logoutButton").setVisible(true);
     } else {
         console.log('not found');
-        Ext.getCmp("loginButton").setVisible(true);
-        Ext.getCmp("logoutButton").setVisible(false);
-    }
-    */
+        $(".loginButton").css("display","block");
+        //Ext.getCmp("logoutButton").setVisible(false);
+    }    
     // ?get uid from local storage here?
     //have to reach docked bottom bar, change to sign out, display form links
 }
