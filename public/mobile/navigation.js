2c8e5fc3b47f3cc19e642ece3ebe81e11ff86888
Aaron Hans <ahans@ign.com>
2011-06-26 22:18:57 -0700

    moving towards pretty stars

--- public/mobile/navigation.js
+++ public/mobile/navigation.js
@@ -4,7 +4,24 @@ var backHandler = function(b,e) {
     }
 }
 var rateHandler = function(b,e) {
-    if(b.getText() == "Back") {
-        Ext.getCmp('mainPnl').setActiveItem(0);
+    if(b.getText() == "Rate") {
+        Ext.getCmp('mainPnl').setActiveItem(reviewForm);
     }
+
+    if(b.getText() == "Submit") {
+        reviewForm.submit({
+            url: '/menu_item_ratings',
+            method: 'post',
+            submitDisabled: true,
+            waitMsg: 'Saving Data...Please wait.',
+            success: function (objForm,httpRequest) {
+                var mbox = new Ext.MessageBox({});
+                mbox.alert("Record Saved");
+            },
+            failure: function() {
+                console.log('submissionFailed');
+            }
+        })
+    }
+
 }
