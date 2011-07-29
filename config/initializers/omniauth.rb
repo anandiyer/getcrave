1d5f2b2c20b0afe253e3939b87eda203e2864b8c
anandiyer <truecalling9@gmail.com>
2011-07-29 14:24:30 -0700

    dynamically changing the omniauth display based on mobile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -27,3 +27,61 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, APP_ID, APP_SECRET, {:client_options => {:ssl => {:ca_file => "/etc/ssl/certs"}}}
   #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end
+
+module OmniAuth
+  module Strategies
+    class Foursquare < OAuth2
+
+#      MOBILE_USER_AGENTS =  'webos|ipod|iphone|mobile'
+
+      def request_phase
+        options[:display] = mobile_request? ? 'touch' : 'page'
+        super
+      end
+
+      def mobile_request?
+#        ua = Rack::Request.new(@env).user_agent.to_s
+#        ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+
+        Rack::Request.new(@env)
+        @uri = @env['REQUEST_URI']
+
+        if (@uri.include?'mobile')
+          return true
+        else
+          return false
+        end
+      end
+
+    end
+  end
+end
+
+module OmniAuth
+  module Strategies
+    class Facebook < OAuth2
+
+#      MOBILE_USER_AGENTS =  'webos|ipod|iphone|mobile'
+
+      def request_phase
+        options[:display] = mobile_request? ? 'touch' : 'page'
+        super
+      end
+
+      def mobile_request?
+#        ua = Rack::Request.new(@env).user_agent.to_s
+#        ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+
+        Rack::Request.new(@env)
+        @uri = @env['REQUEST_URI']
+
+        if (@uri.include?'mobile')
+          return true
+        else
+          return false
+        end
+      end
+
+    end
+  end
+end
